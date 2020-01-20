0ff637bf8b04adf92d3305f8bdd3e57c4390976a
Leonardo Kaminski Ferreira <leonardo.kaminski.ferreira@gmail.com>
2020-01-20 12:50:45 -0300

    Added repos view controller

---
+++ Itau-iOS/Itau-iOS/Modules/Repositories/ViewControllers/RepositoriesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RepositoriesViewController.swift
+//  Itau-iOS
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 20/01/20.
+//  Copyright © 2020 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+
+    // MARK: - Initialisers
+    
+    init() {
+        super.init(nibName: String(describing: type(of: self)), bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
