df95f4e925814f36b891d204c7d83a572fc9a69e
Leonardo Kaminski Ferreira <leonardo.kaminski.ferreira@gmail.com>
2020-01-22 10:39:30 -0300

    Added base PRs view

---
+++ Itau-iOS/Modules/Pull Requests/ViewControllers/PullRequestsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PullRequestsViewController.swift
+//  Itau-iOS
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 22/01/20.
+//  Copyright © 2020 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class PullRequestsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Itau-iOS/Modules/Pull Requests/Views/PullRequestsListView.swift
@@ -0,0 +1,16 @@
+//
+//  PullRequestsListView.swift
+//  Itau-iOS
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 22/01/20.
+//  Copyright © 2020 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class PullRequestsListView: UIView {
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet private weak var tableView: UITableView!
+
+}
